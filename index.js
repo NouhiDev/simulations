88d135b173fcd6e24990b1e772c5ff4f6394024a
nouhi <wolfgraphics.business@gmail.com>
2023-06-20 20:53:30 +0200

    Simulations Page Added

--- index.js
+++ index.js
@@ -20,22 +20,10 @@ function delay(time) {
   return new Promise((resolve) => setTimeout(resolve, time));
 }
 
-function back_btn() {
-  location.href = "https://nouhidev.github.io/projects";
+function ep_btn() {
+  location.href = "https://nouhidev.github.io/epidemic-simulation/";
 }
 
-function remnants_btn() {
-  location.href = "https://nouhidev.github.io/remnants/";
-}
-
-function sb_btn() {
-  location.href = "https://nouhidev.itch.io/shreks-basement";
-}
-
-function achromatopsia_btn() {
-  location.href = "https://nouhidev.itch.io/achromatopsia";
-}
-
-function ets_btn() {
-  location.href = "https://nouhidev.itch.io/escape-the-shrek";
+function ts_btn() {
+  location.href = "https://nouhidev.github.io/traffic-simulation";
 }
