d6050098f8e71872b88922acd28cbae47459f8e6
nouhi <wolfgraphics.business@gmail.com>
2023-07-20 21:25:06 +0200

    centralized

---
+++ ndev-site.js
@@ -0,0 +1,23 @@
+// ░█▄─░█ ░█▀▀▄ ░█▀▀▀ ░█──░█ 　 ░█▀▀▀█ ▀█▀ ▀▀█▀▀ ░█▀▀▀ 　 ───░█ ░█▀▀▀█ 
+// ░█░█░█ ░█─░█ ░█▀▀▀ ─░█░█─ 　 ─▀▀▀▄▄ ░█─ ─░█── ░█▀▀▀ 　 ─▄─░█ ─▀▀▀▄▄ 
+// ░█──▀█ ░█▄▄▀ ░█▄▄▄ ──▀▄▀─ 　 ░█▄▄▄█ ▄█▄ ─░█── ░█▄▄▄ 　 ░█▄▄█ ░█▄▄▄█
+
+// Site-specific JavaScript for their respective individual needs
+
+// ! Requires ndev-global.js to function correctly ! (https://nouhi.dev/assets/global-scripts/ndev-global.js)
+
+function ep_btn() {
+    location.href = "https://nouhidev.github.io/epidemic-simulation/";
+}
+
+function ts_btn() {
+    location.href = "https://nouhidev.github.io/traffic-simulation";
+}
+
+function as_btn() {
+    location.href = "https://nouhidev.github.io/archimedes-screw-simulation";
+}
+
+function es_btn() {
+    location.href = "https://nouhidev.github.io/ecosystem-simulation/";
+}
