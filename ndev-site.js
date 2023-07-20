51f2069418d4ae585f1ff229520044f8e4592577
nouhi <wolfgraphics.business@gmail.com>
2023-07-20 21:28:40 +0200

    Update ndev-site.js

--- ndev-site.js
+++ ndev-site.js
@@ -6,6 +6,17 @@
 
 // ! Requires ndev-global.js to function correctly ! (https://nouhi.dev/assets/global-scripts/ndev-global.js)
 
+document.getElementById("cards").onmousemove = (e) => {
+    for (const card of document.getElementsByClassName("card")) {
+        const rect = card.getBoundingClientRect(),
+            x = e.clientX - rect.left,
+            y = e.clientY - rect.top;
+
+        card.style.setProperty("--mouse-x", `${x}px`);
+        card.style.setProperty("--mouse-y", `${y}px`);
+    }
+};
+
 function ep_btn() {
     location.href = "https://nouhidev.github.io/epidemic-simulation/";
 }
